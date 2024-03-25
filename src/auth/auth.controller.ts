c4acc9a061a577b856136f233e7272e8d49a1cba
Cong Minh <ndmcsystem176@gmail.com>
2024-03-25 14:58:13 +0700

    role-decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,11 +2,14 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDTO, RegisterDTO } from './dto';
 import { User } from '@prisma/client';
+import { Role } from 'src/role/role.enum';
+import { Roles } from 'src/role/role.decorator';
 
 @Controller('auth')
 export class AuthController {
 	constructor(private authService: AuthService){}
 	@Post('register')
+	@Roles(Role.Admin)
 	register(@Body() body : RegisterDTO):Promise<User>{
 		return this.authService.register(body)
 	}
--- src/role/role.decorator.ts
+++ src/role/role.decorator.ts
@@ -2,4 +2,6 @@ import { SetMetadata } from '@nestjs/common';
 import { Role } from './role.enum';
 
 export const ROLES_KEY = 'roles';
+//Setmetadata: không cần import hay gì cả , chỉ cần khai báo như này => cứ @Roles mà dùng thôi 
+//uy tín
 export const Roles = (...roles: Role[]) => SetMetadata(ROLES_KEY, roles);
