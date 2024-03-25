8b834466a500670b0a695d5c9ca9dc9445c5a1ee
Cong Minh <ndmcsystem176@gmail.com>
2024-03-25 17:16:37 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-let port = process.env.PORT
+const port = process.env.PORT
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(port,()=>{
@@ -9,4 +9,6 @@ async function bootstrap() {
   });
 }
 bootstrap();
-//authen
+//tạo 1 lớp role.guard để so sánh các role được gán cho người dùng hiện tại với các role
+//được gán cho người dùng thực tế được require bởi các route 
+//để truy cập các role của các route được khai báo , sủ dụng các package trong @nestjs/core
