de8202991a2bdc2eef32c7662f20d5513ab488f8
Cong Minh <ndmc.system176@gmail.com>
2024-03-29 16:08:40 +0700

    done guard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,8 +2,7 @@ import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDTO, RegisterDTO } from './dto/auth.dto';
 import { User } from '@prisma/client';
-// import { Role } from 'src/role/role.enum';
-// import { Roles } from 'src/role/role.decorator';
+
 @Controller('auth')
 export class AuthController {
 	constructor(private authService: AuthService){}
@@ -11,7 +10,6 @@ export class AuthController {
 	register(@Body() body : RegisterDTO):Promise<User>{
 		return this.authService.register(body)
 	}
-	
 	@Post('login')
 	login(@Body() body: LoginDTO):Promise<any>{
 		return this.authService.login(body)
