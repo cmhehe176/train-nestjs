aad8ce9bd896ce16924e904de969032ffed34437
Cong Minh <ndmc.system176@gmail.com>
2024-04-26 14:17:37 +0700

    test

--- src/main.ts
+++ src/main.ts
@@ -12,3 +12,5 @@ bootstrap();
 //tạo 1 lớp role.guard để so sánh các role được gán cho người dùng hiện tại với các role
 //được gán cho người dùng thực tế được require bởi các route 
 //để truy cập các role của các route được khai báo , sủ dụng các package trong @nestjs/core
+
+//sadhfjkasdjf
